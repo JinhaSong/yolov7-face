89b02bf6a5ee3056dd9df41981b59e8c68089ede
JinhaSong <jinhasong@sogang.ac.kr>
2023-06-13 17:14:41 +0900

    Update YOLOv7Face class

--- yolov7_face.py
+++ yolov7_face.py
@@ -84,6 +84,51 @@ class YOLOv7Face(Detection):
                 y1 = int(xywh[1] * h - 0.5 * xywh[3] * h)
                 x2 = int(xywh[0] * w + 0.5 * xywh[2] * w)
                 y2 = int(xywh[1] * h + 0.5 * xywh[3] * h)
-                boxes.append(["face", class_num, conf, x1, y1, x2 - x1, y2 - y1])
+                if w > 10 and h > 10:
+                    boxes.append(["face", class_num, conf, x1, y1, x2 - x1, y2 - y1])
 
         return boxes
+
+    def detect_batch(self, images:list):
+        results = []
+        tensor_images = []
+        shapes = []
+        stride = int(self.model.stride.max())
+        for image in images:
+            shapes.append([[image.shape[0], image.shape[1]], [[0.3333333333333333, 0.3333333333333333], [16.0, 12.0]]])
+            image = letterbox(image, self.image_size, stride=stride)[0]
+            image = image[:, :, ::-1].transpose(2, 0, 1)
+            image = numpy.ascontiguousarray(image)
+            tensor_images.append(torch.from_numpy(image))
+
+        targets =  torch.zeros((0, 6))
+        image = torch.stack(tensor_images, 0)
+        image = image.to(self.device, non_blocking=True)
+        image = image.float()  # uint8 to fp16/32
+        image /= 255.0  # 0 - 255 to 0.0 - 1.0
+        targets = targets.to(self.device)
+        nb, _, height, width = image.shape  # batch size, channels, height, width
+
+        with torch.no_grad():
+            out, __ = self.model(image, augment=False)
+            targets[:, 2:] *= torch.Tensor([width, height, width, height]).to(self.device)  # to pixels
+            labels = [targets[targets[:, 0] == i, 1:] for i in range(nb)]
+            out = non_max_suppression(out, conf_thres=self.conf_thresh, iou_thres=self.iou_thresh, labels=labels, multi_label=True)
+
+        for si, det in enumerate(out):
+            result = []
+            if len(det):
+                detn = det.clone()
+                detn[:, :4] = scale_coords(image[si].shape[1:], detn[:, :4], tuple(shapes[si][0])).round()
+                for *xyxy, conf, cls in reversed(detn.tolist()):
+                    if conf > self.conf_thresh:
+                        conf = float(conf)
+                        x = float(xyxy[0])
+                        y = float(xyxy[1])
+                        w = float(xyxy[2]) - x
+                        h = float(xyxy[3]) - y
+                        if w > 10 and h > 10:
+                            result.append(["face", int(cls), conf, x, y, w, h])
+            results.append(result)
+
+        return results
